462f526131b4336718ed27aa256e4f052f2643f4
Raph Wilson Del Villar <raphdelvillar@gmail.com>
2023-01-10 23:53:30 +0800

    update mapping account providers

--- Blitz-API/mapping/src/account/accounts.module.ts
+++ Blitz-API/mapping/src/account/accounts.module.ts
@@ -4,11 +4,12 @@ import { MappingsService } from 'src/mapping/mappings.service';
 import { AuthenticationService } from 'src/service/authentication.service';
 import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
+import * as Accounts from 'src/service/account.service';
 
 @Module({
     imports: [HttpModule],
     exports: [],
     controllers: [AccountsController],
-    providers: [AccountsService, MappingsService, ConfigurationsService, AuthenticationService]
+    providers: [AccountsService, MappingsService, ConfigurationsService, AuthenticationService, Accounts.AccountsService]
 })
 export class AccountsModule { }
