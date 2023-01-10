c3598342bf5c5477b0bb45d12352c05f4f0309ab
Raph Wilson Del Villar <raphdelvillar@gmail.com>
2023-01-10 23:51:53 +0800

    add accounts service to the required providers

--- Blitz-API/integration/src/salesforce/salesforces.module.ts
+++ Blitz-API/integration/src/salesforce/salesforces.module.ts
@@ -4,11 +4,12 @@ import { SalesforcesController } from './salesforces.controller';
 import { SalesforcesService } from './salesforces.service';
 import { SalesforcesConnService } from './salesforces.conn.service';
 import { AuthenticationService } from 'src/service/authentication.service';
+import { AccountsService } from 'src/service/account.service';
 
 @Module({
     imports: [HttpModule],
     exports: [],
     controllers: [SalesforcesController],
-    providers: [SalesforcesService, SalesforcesConnService, ConfigurationsService, AuthenticationService]
+    providers: [SalesforcesService, SalesforcesConnService, ConfigurationsService, AuthenticationService, AccountsService]
 })
 export class SalesforcesModule { }
