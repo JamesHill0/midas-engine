d1be2ae1e7ea68230c1fe2125a551756a472f75f
Raph Wilson Del Villar <raphdelvillar@gmail.com>
2023-01-17 22:10:56 +0800

    update providers for logger

--- Blitz-API/logger/src/apps/apps.module.ts
+++ Blitz-API/logger/src/apps/apps.module.ts
@@ -2,11 +2,13 @@ import { HttpModule, Module } from '@nestjs/common';
 import { AppsController } from './apps.controller';
 import { AppsService } from './apps.service';
 import { ConfigurationsService } from 'src/configurations/configurations.service';
+import { AuthenticationService } from 'src/service/authentication.service';
+import { AccountsService } from 'src/accounts/accounts.service';
 
 @Module({
     imports: [HttpModule],
     exports: [],
     controllers: [AppsController],
-    providers: [AppsService, ConfigurationsService]
+    providers: [AppsService, AuthenticationService, AccountsService, ConfigurationsService]
 })
 export class AppsModule { }
